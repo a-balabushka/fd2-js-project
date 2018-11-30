799e15a69963283cb0692e2bc65db92ff4f5ca1f
Alexsey Balabushko <pro100alexsey97@mail.ru>
2018-11-30 21:24:09 +0200

    Change sprite. Modify levels and logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const tileSize = 16; // tile size
-const mapSize = 26; // number of tiles
-// 0: brick, 1: steel, 2: water, 3: tree, 4: border, 5: blank
+const mapSize = 28; // number of tiles
+const tileSize = 22;
 
 let map;
 let tankAmount;
@@ -20,10 +19,6 @@ const sprite = new Image();
 sprite.onload = drawGame;
 sprite.src = 'img/sprites.png';
 
-const trees = new Image();
-trees.onload = drawGame;
-trees.src = 'img/trees.png';
-
 /* --------------------------------------------------------------- */
 
 class Tank {
@@ -39,7 +34,7 @@ class Tank {
     this.spriteY = spriteY;
     this.counter = 0;
     this.size = tileSize * 2;
-    this.positionView = 1;
+    this.positionView = 0;
     this.shooting = false;
 
     if (this.bot) {
@@ -79,40 +74,25 @@ class Tank {
   up() {
     this.speedY = -this.speed;
     this.speedX = 0;
-    this.positionView = 1;
+    this.positionView = 0;
   }
 
   right() {
     this.speedX = this.speed;
     this.speedY = 0;
-    this.positionView = 2;
+    this.positionView = 1;
   }
 
   down() {
     this.speedY = this.speed;
     this.speedX = 0;
-    this.positionView = 3;
+    this.positionView = 2;
   }
 
   left() {
     this.speedX = -this.speed;
     this.speedY = 0;
-    this.positionView = 4;
-  }
-
-  checkGoingBeyond() {
-    if (this.posY + this.size > canvas.height) {
-      this.posY = canvas.height - this.size;
-    }
-    if (this.posY < 0) {
-      this.posY = 0;
-    }
-    if (this.posX + this.size > canvas.width) {
-      this.posX = canvas.width - this.size;
-    }
-    if (this.posX < 0) {
-      this.posX = 0;
-    }
+    this.positionView = 3;
   }
 
   obstaclesCheck() {
@@ -124,7 +104,7 @@ class Tank {
       const part1 = map[curPosY][i];
       const part2 = map[curPosY + 1][i];
 
-      if ((part1 === 0 || part1 === 1) || (part2 === 0 || part2 === 1)) {
+      if ((part1 >= 0 && part1 <= 2) || (part2 >= 0 && part2 <= 2)) {
         if (this.posX < (i + 1) * tileSize) {
           this.posX = (i + 1) * tileSize;
         }
@@ -135,7 +115,7 @@ class Tank {
     for (let i = curPosX; i <= map[curPosY].length; i++) {
       const part1 = map[curPosY][i];
       const part2 = map[curPosY + 1][i];
-      if ((part1 === 0 || part1 === 1) || (part2 === 0 || part2 === 1)) {
+      if ((part1 >= 0 && part1 <= 2) || (part2 >= 0 && part2 <= 2)) {
         if (this.posX > (i - 2) * tileSize) {
           this.posX = (i - 2) * tileSize;
         }
@@ -146,7 +126,7 @@ class Tank {
     for (let i = 0; i <= curPosY; i++) {
       const part1 = map[i][curPosX];
       const part2 = map[i][curPosX + 1];
-      if ((part1 === 0 || part1 === 1) || (part2 === 0 || part2 === 1)) {
+      if ((part1 >= 0 && part1 <= 2) || (part2 >= 0 && part2 <= 2)) {
         if (this.posY < (i + 1) * tileSize) {
           this.posY = (i + 1) * tileSize;
           this.positionView = 1;
@@ -158,7 +138,7 @@ class Tank {
     for (let i = curPosY; i < map.length; i++) {
       const part1 = map[i][curPosX];
       const part2 = map[i][curPosX + 1];
-      if ((part1 === 0 || part1 === 1) || (part2 === 0 || part2 === 1)) {
+      if ((part1 >= 0 && part1 <= 2) || (part2 >= 0 && part2 <= 2)) {
         if (this.posY > (i - 2) * tileSize) {
           this.posY = (i - 2) * tileSize;
         }
@@ -189,42 +169,16 @@ class Tank {
     this.counter++;
     this.posX += this.speedX;
     this.posY += this.speedY;
-    this.checkGoingBeyond();
     this.obstaclesCheck();
     this.deathUser();
   }
 
   animation() {
+    let s = this.bot ? 4 : 3;
     if (this.speedX !== 0 || this.speedY !== 0) {
-      switch (this.positionView) {
-        case 1:
-          this.spriteX = Math.floor(this.counter / 3 % 2) * 35 + 210;
-          break;
-        case 2:
-          this.spriteX = Math.floor(this.counter / 3 % 2) * 35 + 140;
-          break;
-        case 3:
-          this.spriteX = Math.floor(this.counter / 3 % 2) * 35;
-          break;
-        case 4:
-          this.spriteX = Math.floor(this.counter / 3 % 2) * 35 + 70;
-          break;
-      }
+      this.spriteX = Math.floor(this.counter / s % 2) * 35 + (70 * this.positionView);
     } else {
-      switch (this.positionView) {
-        case 1:
-          this.spriteX = 210;
-          break;
-        case 2:
-          this.spriteX = 140;
-          break;
-        case 3:
-          this.spriteX = 0;
-          break;
-        case 4:
-          this.spriteX = 70;
-          break;
-      }
+      this.spriteX = 70 * this.positionView;
     }
   }
 
@@ -239,19 +193,12 @@ class Bullet {
   constructor(x, y, positionView, bot, id) {
     this.id = id;
     this.bot = bot;
-    this.posX = x + 12;
-    this.posY = y + 12;
-    this.size = 8;
+    this.posX = x + 17;
+    this.posY = y + 17;
+    this.size = 10;
     this.speed = 5;
     this.positionView = positionView;
     this.counter = 0;
-  }
-
-  checkGoingBeyond() {
-    if (this.posY < 0 || this.posY - this.size > canvas.height
-      || this.posX < 0 || this.posX - this.size > canvas.width) {
-      bulletsArr.splice(bulletsArr.indexOf(this), 1);
-    }
   }
 
   collisionDetection() {
@@ -267,7 +214,7 @@ class Bullet {
           this.animationHit();
         }
       }
-      if (blockY === 1) {
+      if (blockY === 1 || blockY === 2) {
         if (bposY < (i + 1)) {
           bulletsArr.splice(bulletsArr.indexOf(this), 1);
           this.animationHit();
@@ -320,20 +267,19 @@ class Bullet {
 
   update() {
     switch (this.positionView) {
-      case 1:
+      case 0:
         this.posY -= this.speed;
         break;
-      case 2:
+      case 1:
         this.posX += this.speed;
         break;
-      case 3:
+      case 2:
         this.posY += this.speed;
         break;
-      case 4:
+      case 3:
         this.posX -= this.speed;
         break;
     }
-    this.checkGoingBeyond();
     this.collisionDetection();
     this.killBot();
     this.deathUser();
@@ -341,17 +287,17 @@ class Bullet {
   }
 
   draw() {
-    context.drawImage(sprite, 45, 80, 8, 8, this.posX, this.posY, 8, 8);
+    context.drawImage(sprite, this.positionView * 15, 120, 8, 8, this.posX, this.posY, this.size, this.size);
     this.update();
   }
 }
 
 const eagle = {
-  posX: 192,
-  posY: 384,
+  posX: 286,
+  posY: 550,
   size: tileSize * 2,
   draw() {
-    context.drawImage(sprite, 105, 100, 32, 32, this.posX, this.posY, this.size, this.size);
+    context.drawImage(sprite, 200, 120, 32, 32, this.posX, this.posY, this.size, this.size);
   },
 };
 
@@ -379,7 +325,7 @@ function isColliding(a, b) {
 
 function init() {
   map = Array.from(levels.level1);
-  userTank = new Tank(144, 384, 2, 0, false, 100);
+  userTank = new Tank(220, 550, 2, 0, false, 100);
   botTanksArr = [];
   tankAmount = 5;
 }
@@ -388,7 +334,7 @@ function init() {
 
 function startGame() {
   window.requestAnimationFrame(tick);
-  setInterval(generateBotTank, 5000);
+  setInterval(generateBotTank, 2500);
 }
 
 function tick() {
@@ -401,7 +347,17 @@ function generateBotTank() {
   if ((tankAmount !== 0) && (botTanksArr.length < 5)) {
     const x = Math.floor(Math.random() * (2 + 1));
     idCount += 1;
-    botTanksArr.push(new Tank(x * 200, 0, 1, 40, true, idCount));
+    switch (x) {
+      case 0:
+        botTanksArr.push(new Tank(22, 22, 1, 40, true, idCount));
+        break;
+      case 1:
+        botTanksArr.push(new Tank(290, 22, 1, 40, true, idCount));
+        break;
+      case 2:
+        botTanksArr.push(new Tank(550, 22, 1, 40, true, idCount));
+        break;
+    }
     tankAmount--;
   }
 }
@@ -442,13 +398,17 @@ function drawMap() {
     for (let x = 0; x < mapSize; x++) {
       switch (map[y][x]) {
         case 0:
-          context.drawImage(sprite, 60, 80, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
+          context.drawImage(sprite, 75, 120, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
           break;
         case 1:
-          context.drawImage(sprite, 85, 80, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
+          context.drawImage(sprite, 100, 120, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
+          break;
+        case 2:
+          context.fillStyle = 'dimgrey';
+          context.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
           break;
         case 3:
-          context.drawImage(trees, 0, 0, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
+          context.drawImage(sprite, 150, 120, 16, 16, x * tileSize, y * tileSize, tileSize, tileSize);
           break;
       }
     }
